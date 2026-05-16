5a2211bde48d58dcd8b6cafc272b44bf49279648
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-05-17 02:44:07 +0530

    fix(App.jsx): resolve React state object rendering issues
    - Fix object state mutations to update the count properly.
    - Fix typos in button onClick handlers (redcick -> redclick).
    - Update default state color to Traffic to avoid text duplication.
    - Add logic to reset the count to 1 when switching between different traffic signal colors.

--- React challenges/userEffect/userEffectHandsOn/src/App.jsx
+++ React challenges/userEffect/userEffectHandsOn/src/App.jsx
@@ -2,7 +2,29 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState({color:"Traffic",val:0})
+
+
+  
+  function redclick(){
+    setCount(prev => ({
+      color: "RED",
+      val: prev.color === "RED" ? prev.val + 1 : 1
+    }));
+  }  
+  function greenclick(){
+    setCount(prev => ({
+      color: "GREEN",
+      val: prev.color === "GREEN" ? prev.val + 1 : 1
+    }));
+  }  
+  function yellowclick(){
+    setCount(prev => ({
+      color: "YELLOW",
+      val: prev.color === "YELLOW" ? prev.val + 1 : 1
+    }));
+  }
+
 
   return (
   
@@ -18,19 +40,19 @@ function App() {
 <div className="signal flex gap-13 items-center justify-center">
    <h3 className='text-3xl text-shadow-white-200 text-center uppercase font-stretch-semi-condensed '>Trafic signal</h3>
 
-   <button className='click bg-black p-2 rounded-2xl    '>
+   <button className='click bg-black p-2 rounded-2xl     ' onClick={greenclick}>
     
 <p className='bg-green-500 p-4 text-white uppercase  rounded-4xl'>Green</p>
     </button> 
     
-      <button className='click bg-black p-2 rounded-2xl   '>
+      <button className='click bg-black p-2 rounded-2xl ' onClick={yellowclick}>
     
 
 <p className='bg-yellow-400 uppercase p-4 rounded-4xl'>YELLOW</p>
 
     </button>   
     
-    <button className='click bg-black p-2 rounded-2xl '>
+    <button className='click bg-black p-2 rounded-2xl ' onClick={redclick}>
     
 <p className='bg-red-500 p-4  rounded-4xl'>RED</p>
     </button>
@@ -38,9 +60,17 @@ function App() {
 </div>
 </div>
   </div>
-  <div className="main w-70 h-20 bg-gray-400 rounded-4xl text-2xl text-gray-200  relative ">
-    <p className='text-2xl '></p>
-  </div>
+  <div className="main w-70 h-20 bg-gray-400 rounded-4xl text-2xl text-gray-200 text-center  gap-1.5">
+    <p className='text-2xl text-black font-bold uppercase '>{count.color} signal</p>
+<div className=' main w-70 h-20 bg-gray-600 rounded-4xl text-2x text-center flex justify-center items-center'>
+<p className='text-3xl font-extrabold'>{count.val}</p>
+</div>
+  
+  </div>  
+
+
+
+
     </div>
     
 
