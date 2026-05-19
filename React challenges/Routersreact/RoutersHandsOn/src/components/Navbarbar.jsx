54359fbca8b2471d9fb746a09a75b0e9b430664b
prem-nj <premjadhav21314@gmail.com>
2026-05-19 16:30:26 +0530

    navbar created

---
+++ React challenges/Routersreact/RoutersHandsOn/src/components/Navbarbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbarbar = () => {
+  return (
+    <div className=' bg-amber-200 flex '>
+  <ul>
+
+        <li>HOME</li>
+        <li>About</li>
+        <li>Contacts</li>
+        <li>Dashboard</li>
+
+  </ul>
+
+
+    </div>
+ 
+  )
+}
+
+export default Navbarbar
