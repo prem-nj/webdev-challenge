4fc68925446b33b5fa4331a7eefae43c9f099192
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-05-19 09:59:59 +0530

    devlop ui  componenet for counter

---
+++ React challenges/userContext/HandsOn2/src/components/ChildA.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ChildB from '../../../userContextHandsOn/src/components/ChildB'
+
+const ChildA = () => {
+  return (
+    <ChildB/>
+)
+}
+
+export default ChildA
