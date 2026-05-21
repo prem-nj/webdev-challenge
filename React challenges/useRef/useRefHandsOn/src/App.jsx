f6c9235a32583b0c49cb391fc67d43222c078671
prem-nj <premjadhav21314@gmail.com>
2026-05-21 16:35:03 +0530

    Feat: restart,startbutton,stopbutton event handling of timmer

--- React challenges/useRef/useRefHandsOn/src/App.jsx
+++ React challenges/useRef/useRefHandsOn/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState ,useRef} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -6,25 +6,52 @@ import './App.css'
 import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [isclicked, setisclicked] = useState(false)
+ const timeref = useRef(null)
+  
+function startcount() {
+  timeref.current=setInterval(() => {
+    setCount(count => count + 1);
+  }, 1000);
+  setisclicked(true)
+}
 
+function restartcount(){
+
+  clearInterval(timeref.current);
+  setCount(0);
+      timeref.current = null
+  setisclicked(true)
+
+}
+function stopcount(){
+clearInterval(timeref.current);
+    timeref.current = null
+
+  setisclicked(true)
+}
+
+
+let btnref=useRef();
   return (
     <div className='w-screen h-screen bg-gray-500'>
 
       <Navbar />
+      
       <div className='w-full h-full flex flex-col items-center justify-center'>
 
         <div className="timmer w-1/2 h-1/2 bg-gray-400 rounded-2xl relative flex items-center justify-center">
 
           <div className='absolute left-1/2 -translate-x-1/2 -top-7 bg-amber-50 w-1/3  py-14 shadow text-center rounded-3xl'>
-           <span className=' text-6xl'>3</span> 
+           <span className=' text-6xl'>{count}</span> 
           </div>
          <div className="btn flex  gap-11.5 absolute top-54">
 
 
-         <button className=' start p-3 bg-gray-700   rounded-3xl text-2xl  bottom-1 text-white uppercase'>start</button>
-        <button className='stop p-3 bg-gray-700 text-2xl  rounded-3xl bottom-1 text-white uppercase'>stop</button>
-         <button className=' restart p-3 bg-gray-700 text-2xl rounded-3xl  bottom-1 text-white uppercase'>Restart</button>
+         <button className=' start p-3 bg-gray-700   rounded-3xl text-2xl  bottom-1 text-white uppercase ' onClick={startcount}>start</button>
+        <button className='stop p-3 bg-gray-700 text-2xl  rounded-3xl bottom-1 text-white uppercase' onClick={stopcount} ref={btnref}>stop</button>
+         <button className=' restart p-3 bg-gray-700 text-2xl rounded-3xl  bottom-1 text-white uppercase' onClick={restartcount}>Restart</button>
          </div>
          
         </div>
